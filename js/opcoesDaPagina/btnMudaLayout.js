e128bef89fb2e26c7ca6f77a786f9d820ce9a89b
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-04-19 11:51:14 -0300

    Exercício 2. Cap.9

---
+++ js/opcoesDaPagina/btnMudaLayout.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('#btnMudaLayout')
+
+function mudaTexto() {
+    if(btn.textContent == 'Blocos'){
+        btn.textContent = 'Linhas'
+    }
+    else{
+        btn.textContent = 'Blocos'
+    }
+}
+
+btn.onclick = mudaTexto
