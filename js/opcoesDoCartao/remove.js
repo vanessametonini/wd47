90be4fe872fbf3540a469356eb82fca6d2e09ed3
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-04-20 16:46:03 -0300

    Exercício 6. Cap.13

--- js/opcoesDoCartao/remove.js
+++ js/opcoesDoCartao/remove.js
@@ -1,11 +1,13 @@
 ;(function() {
-    const btn = document.querySelector('.opcoesDoCartao-remove')
-    
-    btn.addEventListener('click', function() {
-        const cartao = btn.parentNode.parentNode
-        cartao.classList.add('cartao--some')
-        cartao.addEventListener('transitionend', function(params) {
-            cartao.remove()
+    const btns = document.querySelectorAll('.opcoesDoCartao-remove')
+
+    for(let i = 0; i < btns.length; i++){
+        btns[i].addEventListener('click', function () {
+            const cartao = btns[i].parentNode.parentNode
+            cartao.classList.add('cartao--some')
+            cartao.addEventListener('transitionend', function (params) {
+                cartao.remove()
+            })
         })
-    })
+    }
 })()
