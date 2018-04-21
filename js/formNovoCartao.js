322ee53b9ef7804a8cd8f2748644ad0b4f9dd9f8
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-04-21 00:38:24 -0300

    Exercício 12. Cap.19

--- js/formNovoCartao.js
+++ js/formNovoCartao.js
@@ -1,5 +1,6 @@
 ;(function() {
     const form = document.querySelector('.formNovoCartao')
+    let numeroDoCartao = 0
 
     form.addEventListener('submit', function(event){
         event.preventDefault()
@@ -21,7 +22,44 @@
 
             form.insertBefore(msgErro, btnSubmit)
 
+        } else {
+            numeroDoCartao++
+            const conteudoDoCartao = textarea.value
+            const cartao = $(`
+            <article tabindex="0" id="cartao_${numeroDoCartao}" class="cartao">
+                <div class="opcoesDoCartao">
+                <button class="opcoesDoCartao-remove opcoesDoCartao-opcao" tabindex="0">
+                    <svg><use xlink:href="#iconeRemover"></use></svg>
+                </button>
+
+                <input type="radio" name="corDoCartao${numeroDoCartao}" value="#EBEF40" id="corPadrão-cartao${numeroDoCartao}" class="opcoesDoCartao-radioTipo" checked>
+                <label for="corPadrão-cartao${numeroDoCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #EBEF40;" tabindex="0">
+                    Padrão
+                </label>
+
+                <input type="radio" name="corDoCartao${numeroDoCartao}" value="#F05450" id="corImportante-cartao${numeroDoCartao}" class="opcoesDoCartao-radioTipo">
+                <label for="corImportante-cartao${numeroDoCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #F05450;" tabindex="0">
+                    Importante
+                </label>
+
+                <input type="radio" name="corDoCartao${numeroDoCartao}" value="#92C4EC" id="corTarefa-cartao${numeroDoCartao}" class="opcoesDoCartao-radioTipo">
+                <label for="corTarefa-cartao${numeroDoCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #92C4EC;" tabindex="0">
+                    Tarefa
+                </label>
+
+                <input type="radio" name="corDoCartao${numeroDoCartao}" value="#76EF40" id="corInspiração-cartao${numeroDoCartao}" class="opcoesDoCartao-radioTipo">
+                <label for="corInspiração-cartao${numeroDoCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #76EF40;" tabindex="0">
+                    Inspiração
+                </label>
+                </div>
+                <p class="cartao-conteudo" contenteditable tabindex="0">${conteudoDoCartao}</p>
+            </article>
+            `)
+
+            $('.mural').append(cartao)
+            textarea.value = ''
         }
+
         
     })
 
